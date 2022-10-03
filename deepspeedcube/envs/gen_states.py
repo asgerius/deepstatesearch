d38a85813507f188f155e388c58cbe41fce7dbb2
asgerius <asger.s@protonmail.com>
2022-10-03 13:21:49 +0200

    Revert "Make sure number of states works"
    This reverts commit cbf319e0bd83d8fcc0637a99f0c89e96f4b517a3.
    
    Read the paper better next time, dingus

--- deepspeedcube/envs/gen_states.py
+++ deepspeedcube/envs/gen_states.py
@@ -11,8 +11,7 @@ from deepspeedcube.envs import Environment
 
 
 def gen_new_states(env: Environment, num_states: int, K: int) -> tuple[torch.Tensor, torch.Tensor]:
-    if num_states % K:
-        num_states = K * (num_states // K)
+    assert num_states % K == 0
     states_per_depth = num_states // K
     log.debug("Generating %s states for %s states per depth" % (thousands_seperators(num_states), thousands_seperators(states_per_depth)))
 
@@ -42,7 +41,7 @@ def gen_new_states(env: Environment, num_states: int, K: int) -> tuple[torch.Ten
             states = states[index].contiguous()
             scramble_depths = scramble_depths[index].contiguous()
 
-    return states, scramble_depths
+    return states[:num_states], scramble_depths[:num_states]
 
 def gen_eval_states(env: Environment, num_states: int, min_scrambles: int, max_scrambles: int) -> tuple[torch.Tensor, list[int]]:
     depths = [random.randint(min_scrambles, max_scrambles) for _ in range(num_states)]
@@ -64,8 +63,8 @@ def get_batches_per_gen(env: Environment, batch_size: int) -> int:
         available_memory = max_mem
 
     # Calculate memory requirements for scrambling
-    state_memory           = 2 * tensor_size(env.get_solved())
-    scramble_depths_memory = 4  # int32
+    state_memory           = tensor_size(env.get_solved())
+    scramble_depths_memory = 2 * 4  # 2 x int32
     actions_memory         = 1  # uint8
     shuffle_index_memory   = 8  # int64
     scramble_memory        = state_memory + scramble_depths_memory\
@@ -74,7 +73,7 @@ def get_batches_per_gen(env: Environment, batch_size: int) -> int:
     # Calculate memory requirements for getting neighbour states
     state_memory     = tensor_size(env.get_solved()) * len(env.action_space)
     actions_memory   = tensor_size(env.action_space)
-    neighbour_memory = state_memory / 2 + actions_memory
+    neighbour_memory = state_memory + actions_memory
 
     total_batch_memory = batch_size * (scramble_memory + neighbour_memory)
     log.debug(
