f4eb670a41be77cf07054c1608a2e2976d6fe455
asgerius <asger.s@protonmail.com>
2022-11-14 16:58:24 +0100

    Reduce allowed amount of memory

--- deepspeedcube/envs/gen_states.py
+++ deepspeedcube/envs/gen_states.py
@@ -54,7 +54,7 @@ def gen_eval_states(env: Environment, num_states: int, min_scrambles: int, max_s
 
 def get_batches_per_gen(env: Environment, batch_size: int) -> int:
     # Only generate up to 20 GB of states, though no more than half the available memory
-    available_memory = 20 * 10 ** 9
+    available_memory = 10 * 10 ** 9
     if available_memory > (max_mem := psutil.virtual_memory().total / 2):
         available_memory = max_mem
 
